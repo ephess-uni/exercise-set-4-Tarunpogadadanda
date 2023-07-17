c4b4651e9d35ff15687b7fe14c85095cba149e59
Tarunpogadadanda <136853344+Tarunpogadadanda@users.noreply.github.com>
2023-07-17 18:20:24 -0500

    ex4

--- src/ex_4_1.py
+++ src/ex_4_1.py
@@ -14,11 +14,12 @@ FILENAME = get_data_file_path('messages.log')
 
 
 def num_shutdowns(logfile):
-    """
-    Your docstring here.  Replace the pass keyword below with your implementation.
-    """
-    pass
-
+    input_file = open(logfile, 'r')
+    output_values = []
+    for line in input_file.readlines():
+        if 'Shutdown complete' in line:
+            output_values.append(line)
+    return len(output_values)
 
 # >>>> The code below will call your function and print the results
 if __name__ == "__main__":
